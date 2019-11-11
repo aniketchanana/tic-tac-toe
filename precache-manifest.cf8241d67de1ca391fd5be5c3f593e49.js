8cf0b807e14cbd7f12fe29f83b585f6bd5fda2f4
Aniket <aniketchanana6@gmail.com>
2019-11-12 01:23:59 +0530

    Updates

---
+++ precache-manifest.cf8241d67de1ca391fd5be5c3f593e49.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "524e39af382596cb39e65cbbeacf7969",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "f5de18d61ac84d469fb1",
+    "url": "/tic-tac-toe/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "5bd943e44dfe9fa8fdba",
+    "url": "/tic-tac-toe/static/css/main.7b52db9e.chunk.css"
+  },
+  {
+    "revision": "f5de18d61ac84d469fb1",
+    "url": "/tic-tac-toe/static/js/2.54100c82.chunk.js"
+  },
+  {
+    "revision": "5bd943e44dfe9fa8fdba",
+    "url": "/tic-tac-toe/static/js/main.3c5a9997.chunk.js"
+  },
+  {
+    "revision": "385629666264212be688",
+    "url": "/tic-tac-toe/static/js/runtime-main.9b00db00.js"
+  }
+]);
